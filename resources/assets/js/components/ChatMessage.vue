81edab86e424d04b24b73a336543f16013b077ba
Funz95 <dav1dtj1n@yahoo.com>
2017-07-27 20:23:48 +0700

    init commit

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p>{{message}}</p>
+		<p>{{user}}</p>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+			message: "This is some message",
+			user: "one"
+			}
+		}
+	}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Chatroom</title>
+	</head>
+	<body>
+	<div id=app>
+		<h1>Chat room</h1>
+		<example></example>
+		<chat-message></chat-message>
+		<chat-log></chat-log>
+		<chat-composer></chat-composer>
+	</div>
+		<script src="js/app.js" charset="utf-8"></script>
+	</body>
+</html>
